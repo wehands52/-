7e73626f927632d6e7a5eef3d86d710ff8446bcb
wehandss <maksdejna47@gmail.com>
2026-01-15 22:45:13 +0300

    Update Циклы.cs

--- Циклы.cs
+++ Циклы.cs
@@ -8,29 +8,6 @@ namespace циклы_while_и_do_while
 {
     internal class Program
     {
-        // В С# имеются 4 вида циклов (for, while, do...while и foreach)
-        // Циклы позволяют выполнить блок кода повторно до тех пор, пока уловлетворятся определенные действия. 
-
-        /*
-        *
-        *while
-        *do while 
-        *for
-        *foreach
-        */
-
-        /* while (условие)
-          {
-            действия цикла;
-          }
-
-
-         //do
-           {
-             действия цикла;
-           }
-          while (условие)
-                          */
         static void Main(string[] args)
         {
             int a = 1;
